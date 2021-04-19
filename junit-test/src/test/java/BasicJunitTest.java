9ea1a2c74d5bcf3e284ee246616eff590935c805
doohyun.kim <danny.kim@sk.com>
2021-04-19 21:56:44 +0900

    No - dependency- test

---
+++ junit-test/src/test/java/BasicJunitTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class BasicJunitTest {
+
+    @Test
+    public void trueTest(){
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void falseTest(){
+        assertEquals(1,1);
+    }
+
+}
